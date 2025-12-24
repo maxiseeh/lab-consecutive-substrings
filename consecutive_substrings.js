ee0fdee390db70b6d14ae59c94677c17d3fb069f
maxiseeh <maxymeeh@gmail.com>
2025-12-24 20:54:53 +0300

    initial commit

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,5 +1,17 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  // make empty array to store results
+  var results = [];
+  
+  // loop through each character as starting point
+  for (var i = 0; i < string.length; i++) {
+    // loop from starting point to end of string
+    for (var j = i; j < string.length; j++) {
+      // get substring and add to results
+      results.push(string.slice(i, j + 1));
+    }
+  }
+  
+  return results;
 }
 
 if (require.main === module) {
